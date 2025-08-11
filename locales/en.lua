bc11ec82216d25ecc917a398ad8f8b404fb5b385
LittlePork1 <140749963+LittlePork1@users.noreply.github.com>
2025-08-11 12:26:33 +0200

    en.lua

---
+++ locales/en.lua
@@ -0,0 +1,9 @@
+Locales = {}
+
+Locales['en'] = {
+    harvest = "Press ~INPUT_CONTEXT~ to harvest tobacco",
+    process = "Press ~INPUT_CONTEXT~ to make cigarettes",
+    sell = "Press ~INPUT_CONTEXT~ to sell your cigarettes",
+    no_items = "You don't have what you need",
+    sold = 'You sold your cigarettes for $%s'
+}
